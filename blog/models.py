43998494b9432818df253469eb20333a828e1547
Pr4v33n23 <praveen.jaisankar@gmail.com>
2020-04-15 00:11:28 +0530

    Blog added in adming, postgresdb added in settings

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=50)
+    blog_pub_date = models.DateTimeField()
+    blog_body = models.TextField()
+    blog_image = models.ImageField(upload_to='blogs/')
