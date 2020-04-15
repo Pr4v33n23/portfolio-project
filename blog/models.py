a14a55bbe59b5052289af476619700f196b5248c
Pr4v33n23 <praveen.jaisankar@gmail.com>
2020-04-16 03:07:15 +0530

    resume, blob, detail blog added. fully functional

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    blog_title = models.CharField(max_length=50)
-    blog_pub_date = models.DateTimeField()
-    blog_body = models.TextField()
-    blog_image = models.ImageField(upload_to='blogs/')
+    title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='blogs/')
+    
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:100]
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y.')
