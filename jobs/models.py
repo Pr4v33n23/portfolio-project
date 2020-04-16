4817dfb3aad10e38b2bc3baa43422dbf8b141749
Pr4v33n23 <praveen.jaisankar@gmail.com>
2020-04-17 00:24:20 +0530

    fixed the jobs title in admin page

--- jobs/models.py
+++ jobs/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Job(models.Model):
-    image_title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    image_title = models.CharField(max_length=50, null=True)
     
     def __str__(self):
         return self.image_title
+
